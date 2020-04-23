08064206a6bf7625dbdcf2a533ca9261e6701bb4
HanyuuuLu <HanyuuFurude@outlook.com>
2020-04-23 23:55:52 +0800

    202004232355

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,29 @@
-import { verbose } from "sqlite3";
-const sqlite3 = verbose();
+import sqlite3 from "sqlite3";
+import fs from "fs";
+const sqlite = sqlite3.verbose();
 const DATABASE_FILE = "container.db";
-export class Record {
+export default class Record {
   Instance = new Record();
-  database = new sqlite3.Database(DATABASE_FILE);
-  init() {}
+  database: sqlite3.Database;
+  constructor() {
+    this.database = new sqlite.Database(DATABASE_FILE);
+    try {
+      if (!fs.existsSync(DATABASE_FILE)) {
+        fs.openSync(DATABASE_FILE, "w");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
   queryContainer(uuid: string): string {
+    this.database.get(
+      `select * from container where uuid==${uuid}`,
+      (err, res) => {
+        res.forEach((element: any) => {
+          console.log(element);
+        });
+      }
+    );
     return "none";
   }
 }
--- src/manager.ts
+++ src/manager.ts
@@ -1,6 +1,9 @@
+import data from "./database";
 class Manager {
   fun() {
     console.log("log");
+    let d = new data();
+    d.queryContainer("1");
   }
 }
 let a = new Manager();
