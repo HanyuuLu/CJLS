2fae55c0985ce783bbe24455a905f28c634fded0
HanyuuLu <HanyuuLu@outlook.com>
2020-04-26 02:20:25 +0800

    202004260220

--- src/database.ts
+++ src/database.ts
@@ -3,52 +3,63 @@ import fs from "fs";
 const DATABASE_FILE = "container.db";
 export default class Record {
   database: sqlite3.Database;
-  query: sqlite3.Statement;
-  grant: sqlite3.Statement;
+  _tableExist: sqlite3.Statement;
+  // _tableCreate: sqlite3.Statement;
+  _query: sqlite3.Statement;
+  _grant: sqlite3.Statement;
+  _queryAll: sqlite3.Statement;
+  _queryCountByUUID: sqlite3.Statement;
+  _insertNewRec: sqlite3.Statement;
+  _gc: sqlite3.Statement;
   constructor() {
+    // try {
+    //   if (!fs.existsSync(DATABASE_FILE)) {
+    //     fs.close(fs.openSync(DATABASE_FILE, "w"), null);
+    //   }
+    // } catch (e) {
+    //   console.error(e);
+    // }
     this.database = sqlite3(DATABASE_FILE);
-    this.query = this.database.prepare("select * from container where uuid=?");
-    this.grant = this.database.prepare(
+    this._tableExist = this.database.prepare(
+      "select count(*) from sqlite_master where type='table' and name='contaier'"
+    );
+
+    this._query = this.database.prepare("select * from container where uuid=?");
+    this._grant = this.database.prepare(
       "insert into container (uuid,container,start,end) values(?,?,?,?)"
     );
-    try {
-      if (!fs.existsSync(DATABASE_FILE)) {
-        fs.openSync(DATABASE_FILE, "w");
-      }
-    } catch (e) {
-      console.error(e);
-    }
-    if (
-      this.database
-        .prepare(
-          "select count(*) from sqlite_master where type='table' and name='contaier'"
-        )
-        .get() == 0
-    ) {
-      this.database
-        .prepare(
-          "CREATE TABLE container (uuid integer,container string,start integer,end integer,primary key(uuid))"
-        )
-        .run();
+    this._queryAll = this.database.prepare("select * from container");
+    this._queryCountByUUID = this.database.prepare(
+      "select count(*) from container where uuid=?"
+    );
+    this._insertNewRec = this.database.prepare(
+      "insert into container (uuid,container,start,end) values(?,?,?,?)"
+    );
+    this._gc = this.database.prepare("delete from container where end<?");
+    if (this._tableExist.get() == 0) {
+      let _tableCreate = this.database.prepare(
+        "CREATE TABLE container (uuid integer,container string,start integer,end integer,primary key(uuid))"
+      );
+      _tableCreate.run();
     }
   }
   queryContainer(uuid: string): object {
-    return this.query.get(uuid);
+    return this._query.get(uuid);
   }
   queryAll(): Array<Object> {
     this.gc();
-    return this.database.prepare("select * from container").all();
+    return this._queryAll.all();
   }
-  grantContainer(uuid: string, time: number) {
+  grantContainer(uuid: string, address: string, time: number): boolean {
     this.gc();
-    let now = Date.now();
-    this.database
-      .prepare(
-        "insert into container (uuid,container,start,end) values(?,?,?,?)"
-      )
-      .run(uuid, "demoIP", now, now + time);
+    if (this._queryCountByUUID.get(uuid) == 0) {
+      let now = Date.now();
+      this._insertNewRec.run(uuid, address, now, now + time);
+      return true;
+    }
+    return false;
   }
   gc() {
-    this.database.prepare("delete from container where end<?").run(Date.now());
+    this._gc.run(Date.now());
   }
 }
--- src/manager.ts
+++ src/manager.ts
@@ -3,13 +3,13 @@ class Manager {
   fun() {
     let d = new data();
     d.queryContainer("1");
+    for (let i = 0; i < 200; ++i) {
+      d.grantContainer((Math.random() * 100).toFixed(), "demo", 1000 * 60);
+    }
     let res = d.queryAll();
     res.forEach((element) => {
       console.log(element);
     });
-    let a = (Math.random() * 1000).toFixed();
-    console.log(a);
-    d.grantContainer(a, 1000 * 60);
   }
 }
 let a = new Manager();
