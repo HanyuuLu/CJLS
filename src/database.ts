2da6216995d0d2f90b19084dcbcac312fadb6a48
HanyuuLu <HanyuuLu@outlook.com>
2020-04-26 16:55:33 +0800

    202004261655

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,19 @@
 import sqlite3 from "better-sqlite3";
 import fs from "fs";
-const DATABASE_FILE = "container.db";
+const DATABASE_FILE = "server.db";
 export default class Record {
   database: sqlite3.Database;
-  _tableExist: sqlite3.Statement;
-  // _tableCreate: sqlite3.Statement;
-  _query: sqlite3.Statement;
-  _grant: sqlite3.Statement;
-  _queryAll: sqlite3.Statement;
-  _queryCountByUUID: sqlite3.Statement;
-  _insertNewRec: sqlite3.Statement;
-  _gc: sqlite3.Statement;
+  _container_query: sqlite3.Statement;
+  _container_grant: sqlite3.Statement;
+  _container_queryAll: sqlite3.Statement;
+  _container_queryCountByUUID: sqlite3.Statement;
+  _container_insertNewRec: sqlite3.Statement;
+  _container_gc: sqlite3.Statement;
+  _user_add: sqlite3.Statement;
+  _user_update: sqlite3.Statement;
+  _user_delete: sqlite3.Statement;
+  _user_queryByUUID: sqlite3.Statement;
+  _user_queryAll: sqlite3.Statement;
   constructor() {
     // try {
     //   if (!fs.existsSync(DATABASE_FILE)) {
@@ -20,46 +23,73 @@ export default class Record {
     //   console.error(e);
     // }
     this.database = sqlite3(DATABASE_FILE);
-    this._tableExist = this.database.prepare(
-      "select count(*) from sqlite_master where type='table' and name='contaier'"
-    );
-
-    this._query = this.database.prepare("select * from container where uuid=?");
-    this._grant = this.database.prepare(
-      "insert into container (uuid,container,start,end) values(?,?,?,?)"
-    );
-    this._queryAll = this.database.prepare("select * from container");
-    this._queryCountByUUID = this.database.prepare(
-      "select count(*) from container where uuid=?"
-    );
-    this._insertNewRec = this.database.prepare(
-      "insert into container (uuid,container,start,end) values(?,?,?,?)"
-    );
-    this._gc = this.database.prepare("delete from container where end<?");
-    if (this._tableExist.get() == 0) {
-      let _tableCreate = this.database.prepare(
-        "CREATE TABLE container (uuid integer,container string,start integer,end integer,primary key(uuid))"
-      );
-      _tableCreate.run();
-    }
+    this.database.exec(`
+      begin;
+      create table if not exists container
+        (
+          uuid      integer primary key,
+          container string not null,
+          start     integer not null,
+          end       integer not null
+        );
+      create table if not exists user
+        (
+          uuid      integer primary key,
+          username  text,
+          level     integer not null default 0,
+          lastused  integer
+        );
+      commit;
+      `);
+    this._container_query = this.database.prepare(`
+      select *
+      from container
+      where uuid=?
+      `);
+    this._container_grant = this.database.prepare(`
+      insert into container 
+      (uuid,container,start,end) 
+      values(?,?,?,?)
+      `);
+    this._container_queryAll = this.database.prepare(`
+      select *
+      from container
+    `);
+    this._container_queryCountByUUID = this.database.prepare(`
+      select count(*) as count
+      from container 
+      where uuid=?
+    `);
+    this._container_insertNewRec = this.database.prepare(`
+      insert into container 
+      (uuid,container,start,end)
+       values(?,?,?,?)
+    `);
+    this._container_gc = this.database.prepare(`
+      delete from container
+      where end<?
+    `);
+    this._user_add = this.database.prepare(`
+      insert into user
+    `)
   }
   queryContainer(uuid: string): object {
-    return this._query.get(uuid);
+    return this._container_query.get(uuid);
   }
   queryAll(): Array<Object> {
     this.gc();
-    return this._queryAll.all();
+    return this._container_queryAll.all();
   }
   grantContainer(uuid: string, address: string, time: number): boolean {
     this.gc();
-    if (this._queryCountByUUID.get(uuid) == 0) {
+    if (this._container_queryCountByUUID.get(uuid).count === 0) {
       let now = Date.now();
-      this._insertNewRec.run(uuid, address, now, now + time);
+      this._container_insertNewRec.run(uuid, address, now, now + time);
       return true;
     }
     return false;
   }
   gc() {
-    this._gc.run(Date.now());
+    this._container_gc.run(Date.now());
   }
 }
