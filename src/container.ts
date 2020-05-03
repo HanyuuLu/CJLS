c2b41a4458f2f6d410e0360018feb9fc9e333270
HanyuuLu <HanyuuLu@outlook.com>
2020-05-03 17:44:27 +0800

    202005031744

--- src/container.ts
+++ src/container.ts
@@ -1,1 +1,58 @@
-export class Container {}
+import Docker from "dockerode";
+import fs from "fs";
+import { Stream } from "stream";
+class Container {
+  docker: Docker;
+  contianerList: Array<Docker.Container>;
+  imageName: string | undefined;
+  constructor() {
+    this.imageName = undefined;
+    try {
+      let config = JSON.parse(fs.readFileSync("config.json").toString());
+      // console.log(config);
+      this.imageName = config["defaultAddress"];
+      console.log(`using image : [${this.imageName}]`);
+    } catch {
+      console.error("Miss config file!");
+    }
+    this.docker = new Docker();
+    this.contianerList = [];
+    this.docker.listImages(function(err, info: Array<any>) {
+      if (err) console.log(err);
+      for (var i = 0; i < info.length; ++i) {
+        for (var tag in info[i]["RepoTags"]) {
+          if (info[i]["RepoTags"][tag] == container.imageName) {
+            console.log(`image [${container.imageName}] ready.`);
+            return;
+          }
+        }
+      }
+      console.warn(`image ${container.imageName} don't exists, pulling...`);
+      container.docker.pull(container.imageName as string, function(
+        err: ExceptionInformation,
+        info: Stream
+      ) {
+        if (err) {
+          throw err.toString();
+        }
+        console.log(info.toString());
+      });
+    });
+  }
+  run() {
+    this.contianerList.push(this.docker.getContainer("4d3e6a4e7bf8"));
+    // this.contianerList[0].start(function(err, data) {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    //   console.log("aaa");
+    //   console.log(data.toString());
+    // });
+    // this.contianerList[0].stop(function(err, data) {
+    //   console.log(err);
+    //   console.log(data?.toString());
+    // });
+  }
+}
+
+export let container = new Container();
--- src/manager.ts
+++ src/manager.ts
@@ -1,15 +1,17 @@
 import data from "./database";
+import { container } from "./container";
 class Manager {
   fun() {
-    let d = new data();
-    d.queryContainer("1");
-    for (let i = 0; i < 200; ++i) {
-      d.grantContainer((Math.random() * 100).toFixed(), "demo", 1000 * 60);
-    }
-    let res = d.queryAll();
-    res.forEach((element) => {
-      console.log(element);
-    });
+    // let d = new data();
+    // d.queryContainer("1");
+    // for (let i = 0; i < 200; ++i) {
+    //   d.grantContainer((Math.random() * 100).toFixed(), "demo", 1000 * 60);
+    // }
+    // let res = d.queryAll();
+    // res.forEach((element) => {
+    //   console.log(element);
+    // });
+    container.run();
   }
 }
 let a = new Manager();
